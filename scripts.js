d87198d44b9a13e0d4f4866b42122de9fbc23f79
Marc-Anthony Mitchell <mitchellmarcus297@gmail.com>
2020-11-01 20:55:38 -0500

    Program Files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
 /* Add your JavaScript to this file */
+
+
+window.onload= ()=>{
+    var p=document.getElementById("email");
+    let b=document.getElementsByClassName("message");
+    let e=document.getElementsByTagName("form");
+    let o=document.getElementsByTagName("button")[0];
+    const entry =(r) => {
+        r.preventDefault();
+        if(p.value===""||p.value==null){
+            alert("Valid Email addresses only!");
+            return false;
+        }else{
+            displayemail();
+            return true
+        }
+    }
+    o.addEventListener("click",entry);
+    function displayemail(){
+        if(p!=null){
+            alert("Thank you!Your email address"+ "" + p.value + "" + "has been added to our mailing list!");
+            p.clear();
+    
+        }
+    }
+
+}
+
+
+
+    
+    
+
+   
+   
